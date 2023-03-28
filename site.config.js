4867c1510044b41fbb45dba0c95d0db0377076b0
halamlee <halam.lee.lami@gmail.com>
2023-03-28 13:15:50 +0900

    :memo: 블로그 기본 설정 수정

--- site.config.js
+++ site.config.js
@@ -1,35 +1,39 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'Lami',
-    image: '/profile.png',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: 'frontend developer',
-    bio: '삶이 온통 사람의 길이니',
-    email: 'halam.lee.lami@gmail.com',
-    linkedin: '',
-    github: 'halamlee',
-    instagram: '',
+    name: "Lami",
+    image: "/profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "frontend developer",
+    bio: "삶이 온통 사람의 길이니",
+    email: "halam.lee.lami@gmail.com",
+    linkedin: "",
+    github: "halamlee",
+    instagram: "",
   },
   projects: [
     {
-      name: 'Easy Gitmoji',
-      href: 'https://easy-gitmoji.halamlee.com/'
-    }
+      name: "Easy Gitmoji",
+      href: "https://easy-gitmoji.halamlee.com/",
+    },
+    {
+      name: "Money For Rabbit",
+      href: "https://money-for-rabbit.netlify.app/",
+    },
   ],
   // blog setting (required)
   blog: {
-    title: 'Lami Log',
-    description: '삶이 온통 사람의 길이니',
-    theme: 'light' // ['light', 'dark', 'auto']
+    title: "삶이 온통 사람의 길이니",
+    description: "삶이 온통 사람의 길이니",
+    theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: 'https://www.halamlee.com/',
+  link: "https://www.halamlee.com/",
   since: 2023, // If leave this empty, current year will be used.
-  lang: 'ko-KR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ["Blog", "Website", "Notion"],
   },
 
   // notion configuration (required)
@@ -41,23 +45,30 @@ const CONFIG = {
   googleAnalytics: {
     enable: false,
     config: {
-      measurementId: process.env.GOOGLE_MEASUREMENT_ID || ''
-    }
+      measurementId: process.env.GOOGLE_MEASUREMENT_ID || "",
+    },
   },
   googleSearchConsole: {
     enable: false,
     config: {
-      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || ''
-    }
+      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
+    },
   },
   utterances: {
     enable: true,
     config: {
-      repo: 'HalamLee/lami-log',
-      'issue-term': 'og:title',
-      label: '💬 Utterances',
-    }
+      repo: "HalamLee/lami-log",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
   },
-  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 }
 module.exports = CONFIG
